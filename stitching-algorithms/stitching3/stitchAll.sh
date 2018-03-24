81cef495b99ee6fc247b642885b31a8cb2157622
Ussama Azam <nuclearoreo@gmail.com>
2018-03-23 20:03:47 -0400

    img2video script and fixed bash script

--- stitching-algorithms/stitching3/stitchAll.sh
+++ stitching-algorithms/stitching3/stitchAll.sh
@@ -1,4 +1,4 @@
 for i in {0..1026};
 do
-  echo ./stitching leftFrames/leftFrame$i.jpg rightFrames/rightFrame$i.jpg --Output stitched$i.jpg
+  ./stitching leftFrames/leftFrame$i.jpg rightFrames/rightFrame$i.jpg --Output stitched$i.jpg;
 done
